e932300d34156a4f0ab15fa39bbdd8c73c331e9f
David Pilato <david@pilato.fr>
2015-02-03 18:31:50 +0100

    Create container when snapshot starts
    For now, Azure container is created when you create the repository.
    It could happen that you manually remove the container using Azure CLI or Azure Console so when you try then to create a new Snapshot it fails.
    
    This commit creates the container if needed when the repository is created or when a snapshot operation is started.
    
    Based on discussion in #53,

--- src/main/java/org/elasticsearch/cloud/azure/blobstore/AzureBlobContainer.java
+++ src/main/java/org/elasticsearch/cloud/azure/blobstore/AzureBlobContainer.java
@@ -21,6 +21,7 @@ package org.elasticsearch.cloud.azure.blobstore;
 
 import com.microsoft.windowsazure.services.core.ServiceException;
 import com.microsoft.windowsazure.services.core.storage.StorageException;
+import org.elasticsearch.ElasticsearchIllegalArgumentException;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.blobstore.BlobMetaData;
 import org.elasticsearch.common.blobstore.BlobPath;
@@ -28,6 +29,7 @@ import org.elasticsearch.common.blobstore.support.AbstractBlobContainer;
 import org.elasticsearch.common.collect.ImmutableMap;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.ESLoggerFactory;
+import org.elasticsearch.repositories.RepositoryException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -45,8 +47,9 @@ public class AzureBlobContainer extends AbstractBlobContainer {
     protected final AzureBlobStore blobStore;
 
     protected final String keyPath;
+    protected final String repositoryName;
 
-    public AzureBlobContainer(BlobPath path, AzureBlobStore blobStore) {
+    public AzureBlobContainer(String repositoryName, BlobPath path, AzureBlobStore blobStore) {
         super(path);
         this.blobStore = blobStore;
         String keyPath = path.buildAsString("/");
@@ -54,6 +57,7 @@ public class AzureBlobContainer extends AbstractBlobContainer {
             keyPath = keyPath + "/";
         }
         this.keyPath = keyPath;
+        this.repositoryName = repositoryName;
     }
 
     @Override
@@ -91,6 +95,8 @@ public class AzureBlobContainer extends AbstractBlobContainer {
             throw new IOException(e);
         } catch (URISyntaxException e) {
             throw new IOException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RepositoryException(repositoryName, e.getMessage());
         }
     }
 
@@ -122,6 +128,11 @@ public class AzureBlobContainer extends AbstractBlobContainer {
             logger.warn("can not access [{}] in container {{}}: {}", prefix, blobStore.container(), e.getMessage());
             throw new IOException(e);
         }
+    }
+
+    @Override
+    public void move(String sourceBlobName, String targetBlobName) throws IOException {
+        throw new ElasticsearchIllegalArgumentException("move() is not implemented yet");
     }
 
     @Override
